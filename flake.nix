4870f337ff1270d682b7c1c4e5a4a04f7570707f
Yann Pelletier <33026902+yannpelletier@users.noreply.github.com>
2025-06-28 09:03:30 -0400

    Added flake

---
+++ flake.nix
@@ -0,0 +1,124 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    astal,
+    ags,
+    ...
+  } @ inputs: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    packages =
+      system: pkgs:
+      (with ags.packages.${system}; [
+        tray
+        hyprland
+        apps
+        battery
+        bluetooth
+        mpris
+        cava
+        network
+        notifd
+        powerprofiles
+        wireplumber
+      ])
+      ++ (with pkgs; [
+        typescript
+        libnotify
+        dart-sass
+        fd
+        btop
+        bluez
+        libgtop
+        gobject-introspection
+        glib
+        bluez-tools
+        grimblast
+        brightnessctl
+        gnome-bluetooth
+        gtksourceview3
+        libsoup_3
+        matugen
+        hyprpicker
+        hyprsunset
+        hypridle
+        wireplumber
+        networkmanager
+        wf-recorder
+        upower
+        gvfs
+        swww
+      ]);
+    inherit (self) outputs;
+  in {
+    packages = forEachSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        default = ags.lib.bundle {
+          inherit pkgs;
+
+          src = ./modules/ags;
+          name = "hyprwonder";
+          entry = "app.ts";
+        };
+      }
+    );
+
+    devShells = forEachSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        default = pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            packages system pkgs
+            ++ [
+              ags.packages.${system}.ags
+              astal.packages.${system}.astal3
+              astal.packages.${system}.io
+              gjs
+              meson
+              pkg-config
+              ninja
+            ];
+          shellHook = ''
+            # Exporting glib-networking modules
+            export GIO_EXTRA_MODULES="${pkgs.glib-networking}/lib/gio/modules"
+            if [ "''${PWD##*/}" = "hyprwonder" ]; then
+              echo "Initialise dependencies required in order for tsserver to work? (y/anything_else)"
+              read consent
+              if [ "$consent" = "y" ]; then
+                ags types -d .; mkdir node_modules; ln -s ${astal.packages.${system}.gjs}/share/astal/gjs ./node_modules/astal
+              fi
+            else
+              echo "You're not in the hyprwonder root directory, initialisation failed"
+            fi
+          '';
+        };
+      }
+    );
+  };
+}
